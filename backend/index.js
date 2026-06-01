a754cd79baee5fa564372db34ee2ef6547b40e0c
Pavanika15 <23eg105v04@anurag.edu.in>
2026-06-01 22:08:46 +0530

    changed CORS

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: "http://localhost:5173", // your frontend URL
+  origin: process.env.CLIENT_URL, // your frontend URL
   credentials: true
 }))
  // Enable All CORS Requests
